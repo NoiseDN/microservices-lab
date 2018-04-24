4b42407b630c5a780dcce554b3e2b7a17e7df1a9
Anton Filimonov <Anton.Filimonov@get.no>
2018-04-24 16:42:32 +0300

    Implement API GW, Zipkit tracing

--- zipkin/src/main/java/com/ogasimov/labs/springcloud/microservices/zipkin/ZipkinApp.java
+++ zipkin/src/main/java/com/ogasimov/labs/springcloud/microservices/zipkin/ZipkinApp.java
@@ -2,8 +2,10 @@ package com.ogasimov.labs.springcloud.microservices.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
+@EnableZipkinStreamServer
 public class ZipkinApp {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApp.class, args);
